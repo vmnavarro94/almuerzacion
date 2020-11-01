553597db1664c16239cf03989f449bead7ee5b30
vnavarro <vnavarro@brightcove.com>
2020-11-01 12:20:44 -0600

    Show Meals detail

--- screens/Modal.js
+++ screens/Modal.js
@@ -1,11 +1,28 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { useFetch } from '../hooks'
 
-export default () => {
-	return(
-		<View>
-			<Text>Modal screen</Text>
+export default ({ navigation }) => {
+	const id = navigation.getParam('_id')
+	const { loading, data } = useFetch(`https://serverless.vnavarro.vercel.app/api/meals/${id}`)
+	return(	
+		<View style={styles.container}>
+			{ loading ? <Text>Cargando...</Text> :
+				<>	
+					<Text>{data._id}</Text>
+					<Text>{data.name}</Text>
+					<Text>{data.desc}</Text>
+				</>
+			}
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
+})
 
